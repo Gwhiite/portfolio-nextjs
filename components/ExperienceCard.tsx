a16026b2a3b860c6f807db6b5dfea655e01fa832
Gwhiite <guibrancodecampos@gmail.com>
2023-11-11 01:11:05 -0300

    fix: tag order

--- components/ExperienceCard.tsx
+++ components/ExperienceCard.tsx
@@ -23,7 +23,7 @@ const ExperienceCard = ({
   return (
     <article className="flex flex-col rounded-lg items-center space-y-6 flex-shrink-0 w-[350px] md:w-[600px] snap-center bg-[#292929] hover:opacity-100 opacity-40 cursor-pointer transition-opacity duration-200 overflow-hidden">
       <div className="p-5">
-        <h4 className="text-3xl font-light">{title}</h4>
+        <h1 className="text-3xl font-light">{title}</h1>
         <p className="font-bold text-2xl mt-1">{company}</p>
         <div className="flex space-x-2 my-2">
           {techs?.map((tech, index) => (
--- components/Project.tsx
+++ components/Project.tsx
@@ -13,7 +13,7 @@ const Project = ({ img, title, source, summary }: Props) => {
     <div className="w-screen flex-shrink-0 snap-center flex flex-col space-y-5 items-center justify-center p-5">
       <Image src={img} alt="project image" width={600} height={300} />
       <div className="space-y-6 px-0 max-w-6xl">
-        <h4 className="text-4xl text-center font-semibold">{title}</h4>
+        <h1 className="text-4xl text-center font-semibold">{title}</h1>
         <p className="text-center">{summary}</p>
         <div className="flex space-x-4 justify-center items-center">
           {source.map((item, index) => (
